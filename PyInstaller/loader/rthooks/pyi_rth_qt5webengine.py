4b63bfa4b705b20b130a3d832742ad5b7bbb692f
Brian Teague <teague@mit.edu>
2018-10-26 21:51:40 -0500

    hooks: make conda qt5 webengine work on windows, linux

--- PyInstaller/loader/rthooks/pyi_rth_qt5webengine.py
+++ PyInstaller/loader/rthooks/pyi_rth_qt5webengine.py
@@ -10,10 +10,34 @@
 import os
 import sys
 
-# See ``pyi_rth_qt5.py`: use a "standard" PyQt5 layout.
 if sys.platform == 'darwin':
     os.environ['QTWEBENGINEPROCESS_PATH'] = os.path.normpath(os.path.join(
-        sys._MEIPASS, '..', 'Resources', 'PyQt5', 'Qt', 'lib',
-        'QtWebEngineCore.framework', 'Helpers', 'QtWebEngineProcess.app',
-        'Contents', 'MacOS', 'QtWebEngineProcess'
+        sys._MEIPASS, 'PyQt5', 'Qt', 'libexec', 'QtWebEngineProcess'
     ))
+
+elif sys.platform.startswith('linux'):
+    os.environ['QTWEBENGINEPROCESS_PATH'] = os.path.normpath(os.path.join(
+        sys._MEIPASS, 'PyQt5', 'Qt', 'libexec', 'QtWebEngineProcess'
+    ))
+    
+    qt_conf_path = os.path.join(sys._MEIPASS, 'PyQt5', 'Qt', 'libexec', 'qt.conf')
+    qt_path = os.path.join(sys._MEIPASS, 'PyQt5', 'Qt')
+    with open(qt_conf_path, 'w') as qt_conf:
+        qt_conf.write('[Paths]\nPrefix = {}\n'.format(qt_path))
+    
+    if not os.path.exists(os.path.normpath(os.path.join(sys._MEIPASS, 'PyQt5', 'Qt', 'lib'))):
+        os.symlink(sys._MEIPASS + '/', os.path.normpath(os.path.join(
+            sys._MEIPASS, 'PyQt5', 'Qt', 'lib')))
+
+elif sys.platform.startswith('win'):
+
+    qt_conf_path = os.path.join(sys._MEIPASS, 'PyQt5', 'Qt', 'bin', 'qt.conf')
+    qt_path = os.path.join(sys._MEIPASS, 'PyQt5', 'Qt')
+
+    # QT's init file format requires front-slashes even on windows
+    if sys.platform.startswith('win'):
+       qt_path = qt_path.replace("\\", '/')
+
+    with open(qt_conf_path, 'w') as qt_conf:
+        qt_conf.write('[Paths]\nPrefix = {}\n'.format(qt_path))
+
