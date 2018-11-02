9ee797078e2145a3fa3c2364f64ae5c468a9d4a4
Brian Teague <bpteague@gmail.com>
2018-11-02 18:05:34 -0500

    Make qt5webengine hook create a qt.conf for darwin and linux

--- PyInstaller/loader/rthooks/pyi_rth_qt5webengine.py
+++ PyInstaller/loader/rthooks/pyi_rth_qt5webengine.py
@@ -10,12 +10,7 @@
 import os
 import sys
 
-if sys.platform == 'darwin':
-    os.environ['QTWEBENGINEPROCESS_PATH'] = os.path.normpath(os.path.join(
-        sys._MEIPASS, 'PyQt5', 'Qt', 'libexec', 'QtWebEngineProcess'
-    ))
-
-elif sys.platform.startswith('linux'):
+if sys.platform == 'darwin' or sys.platform.startswith('linux'):
     os.environ['QTWEBENGINEPROCESS_PATH'] = os.path.normpath(os.path.join(
         sys._MEIPASS, 'PyQt5', 'Qt', 'libexec', 'QtWebEngineProcess'
     ))
